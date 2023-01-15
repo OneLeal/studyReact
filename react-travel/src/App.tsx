4996a2995b743210bf069ca171359f6a3c7b1018
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-15 17:00:00 +0800

    feat: 开发侧边栏组件 feat: 开发走马灯组件

--- react-travel/src/App.tsx
+++ react-travel/src/App.tsx
@@ -1,11 +1,28 @@
 import styles from "./App.module.css";
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
+import { SideMenu } from "./components/sideMenu";
+import { SlideShow } from "./components/slideShow";
+import { Row, Col } from "antd";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+
+      {/* 页面内容 */}
+      <div className={styles["page-content"]}>
+        <Row>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+
+          <Col span={18}>
+            <SlideShow />
+          </Col>
+        </Row>
+      </div>
+
       <Footer />
     </div>
   );

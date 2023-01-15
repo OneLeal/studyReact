7619de7ea07393e5aae186782564bef73be2a1f0
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-16 00:49:50 +0800

    feat: 搭建登录 / 注册 / 详情页面开发目录; feat: 安装 react-router-dom feat: 搭建 pages 目录

--- react-travel/src/App.tsx
+++ react-travel/src/App.tsx
@@ -1,66 +1,10 @@
 import styles from "./App.module.css";
-import { Header } from "./components/header";
-import { Footer } from "./components/footer";
-import { SideMenu } from "./components/sideMenu";
-import { SlideShow } from "./components/slideShow";
-import { HotProducts } from "./components/HotProducts";
-import { Row, Col, Typography } from "antd";
-import { productList1, productList2, productList3 } from "./mock/hotProducts";
-
-import sideImage1 from "./assets/images/sider_2019_12-09.png";
-import sideImage2 from "./assets/images/sider_2019_02-04.png";
-import sideImage3 from "./assets/images/sider_2019_02-04-2.png";
+import { Home } from "./pages/home";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-
-      {/* 页面内容 */}
-      <div className={styles["page-content"]}>
-        <Row>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-
-          <Col span={18}>
-            <SlideShow />
-          </Col>
-        </Row>
-
-        {/* 热门推荐 */}
-        <HotProducts
-          sideImage={sideImage1}
-          list={productList1}
-          title={
-            <Typography.Title level={3} type="warning">
-              爆款推荐
-            </Typography.Title>
-          }
-        />
-
-        <HotProducts
-          sideImage={sideImage2}
-          list={productList2}
-          title={
-            <Typography.Title level={3} type="danger">
-              新品上市
-            </Typography.Title>
-          }
-        />
-
-        <HotProducts
-          sideImage={sideImage3}
-          list={productList3}
-          title={
-            <Typography.Title level={3} type="success">
-              周边旅游
-            </Typography.Title>
-          }
-        />
-      </div>
-
-      <Footer />
+      <Home />
     </div>
   );
 }
---
+++ react-travel/src/pages/travelDetails/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const travelDetails: React.FC = () => {
+  return <div>旅游详情页面</div>;
+};

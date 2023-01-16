458bdc8c7bd35d045fcdbc56790437cf4a3b27f7
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-17 01:01:01 +0800

    feat: 配置 axios

--- react-travel/src/index.tsx
+++ react-travel/src/index.tsx
@@ -6,6 +6,10 @@ import "antd/dist/reset.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./i18n/configs";
+import axios from "axios";
+
+// 设置请求头
+axios.defaults.headers["x-icode"] = "6A2E42F4DD82E0AD"; // 身份验证信息(有效期一个月)
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement

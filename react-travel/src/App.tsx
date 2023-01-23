3f2ca2f00407cdd11d31dced3815dbb9f8a3d027
xiongxiong <15639067881@163.com>
2023-01-23 16:59:54 +0800

    feat: 配置搜索页面路由及获取搜索关键字信息

--- react-travel/src/App.tsx
+++ react-travel/src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from "./pages/home";
 import { SignIn } from "./pages/signIn";
 import { Register } from "./pages/register";
 import { TravelDetails } from "./pages/travelDetails";
+import { SearchPage } from "./pages/search";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/register" element={<Register />} />
           <Route path="/details/:travelId" element={<TravelDetails />} />
+          <Route path="/search/:keywords" element={<SearchPage />} />
           <Route path="*" element={<h1>404 Not Found !</h1>} />
         </Routes>
       </BrowserRouter>
--- react-travel/src/pages/search/index.tsx
+++ react-travel/src/pages/search/index.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import styles from "./index.module.css";
 import { Header } from "../../components/header";
 import { Footer } from "../../components/footer";
+import { useParams } from "react-router-dom";
 
 export const SearchPage: React.FC = () => {
+  const params = useParams();
+  console.log("搜索页面: ", params.keywords);
   return (
     <div>
       <Header></Header>

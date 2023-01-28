5f3cbd4fe81b4c053e0e81bddce8c73f07bd869a
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-28 10:59:13 +0800

    fixed: 解决遗留问题(TS类型检测错误)

--- react-travel/src/pages/home/index.tsx
+++ react-travel/src/pages/home/index.tsx
@@ -1,22 +1,14 @@
 import React from "react";
 import stylesCommon from "../../styles/index.module.css";
-import axios from "axios";
-import { productListApi } from "../../api";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "../../redux/hooks";
+import { useSelector, useAppDispatch } from "../../redux/hooks";
 import { MainLayout } from "../../layouts/main";
 import { SideMenu } from "../../components/sideMenu";
 import { SlideShow } from "../../components/slideShow";
 import { HotProducts } from "../../components/HotProducts";
 import { Row, Col, Typography, Spin } from "antd";
-import {
-  apiActionCreator,
-  fetchRecommendProductsActionCreator,
-  fetchRecommendProductsFailedActionCreator,
-  fetchRecommendProductsSuccessActionCreator,
-} from "../../redux/recommendProducts/recommendProductsActions";
+import { apiActionCreator } from "../../redux/recommendProducts/recommendProductsActions";
 // import {
 //   productList1,
 //   productList2,
@@ -30,7 +22,7 @@ import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
 
 export const Home: React.FC = () => {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const loading = useSelector((state) => state.recommendProducts.loading);
   const error = useSelector((state) => state.recommendProducts.error);
   const productList = useSelector(
@@ -38,23 +30,8 @@ export const Home: React.FC = () => {
   );
 
   useEffect(() => {
-    // dispatch(apiActionCreator()); // FIXME: dispatch 参数类型错误
-
-    // FIXME: 暂时先在组件中进行请求，待上述问题解决后再迁移至中间件调用
-    const http = async () => {
-      try {
-        dispatch(fetchRecommendProductsActionCreator());
-        const { data } = await axios.get(productListApi);
-        dispatch(fetchRecommendProductsSuccessActionCreator(data));
-        console.log("产品推荐列表: ", data);
-      } catch (error) {
-        if (error instanceof Error) {
-          dispatch(fetchRecommendProductsFailedActionCreator(error));
-        }
-      }
-    };
-    http();
-  }, [dispatch]);
+    dispatch(apiActionCreator());
+  }, []); // 可忽略 eslint 提示
 
   if (loading) {
     return <Spin className={stylesCommon["common-spin"]} size="large" />;

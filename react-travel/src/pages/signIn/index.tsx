472e9c3068517e0a55c67ac31a912f4ee732c69f
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-28 15:16:07 +0800

    feat: 搭建登录页面表单组件

--- react-travel/src/pages/signIn/index.tsx
+++ react-travel/src/pages/signIn/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { UserLayout } from "../../layouts/user";
+import { SignInForm } from "./signInForm";
 
 export const SignIn: React.FC = () => {
   return (
     <UserLayout>
-      <h1>登录页面</h1>
+      <SignInForm />
     </UserLayout>
   );
 };
---
+++ react-travel/src/pages/signIn/signInForm.tsx
@@ -0,0 +1,78 @@
+import styles from "./signInForm.module.css";
+import React, { useState } from "react";
+import { Button, Modal, Checkbox, Form, Input } from "antd";
+
+export const SignInForm: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const closeModal = () => {
+    setVisible(false);
+  };
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      <Form
+        className={styles["login-form"]}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+
+      <Modal
+        title="提 示"
+        open={visible}
+        onOk={closeModal}
+        onCancel={closeModal}
+        cancelText={"取 消"}
+        okText={"确 定"}
+        maskClosable={false}
+      >
+        <p>{errorMsg}</p>
+      </Modal>
+    </div>
+  );
+};

cca7ac8e9a69cf93c2b75e9dba244a62572a80d2
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-29 10:21:02 +0800

    feat: 搭建购物车开发目录

--- react-travel/src/pages/shoppingCart/index.tsx
+++ react-travel/src/pages/shoppingCart/index.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import styles from "./index.module.css";
+import { MainLayout } from "../../layouts/main";
+import { ProductList } from "../../components/searchList";
+import { PaymentCard } from "../../components/paymentCard";
+import { Col, Row, Affix } from "antd";
 
 export const ShoppingCart: React.FC = () => {
-  return <div>购物车</div>;
+  return (
+    <MainLayout>
+      <Row>
+        {/* 购物车清单 */}
+        <Col span={16}>
+          <div className={styles["product-list-container"]}>
+            {/* <ProductList /> */}
+          </div>
+        </Col>
+
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <Affix>
+            <div className={styles["payment-card-container"]}>
+              {/* <PaymentCard /> */}
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+    </MainLayout>
+  );
 };

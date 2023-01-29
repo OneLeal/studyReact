458ede0617c626d3a5105e049f7f4c456637d316
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-01-29 16:27:12 +0800

    feat: 新增支付页面组件

--- react-travel/src/pages/payOrder/index.tsx
+++ react-travel/src/pages/payOrder/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { PaymentForm } from "../../components/paymentForm";
+import { CheckOutCard } from "../../components/checkOutCard";
+import { MainLayout } from "../../layouts/main";
+import { Col, Row } from "antd";
 
 export const PayOrder: React.FC = () => {
-  return <div>支付页面</div>;
+  return (
+    <MainLayout>
+      <Row>
+        <Col span={12}>
+          <PaymentForm />
+        </Col>
+        <Col span={12}>
+          {/* <CheckOutCard /> */}
+        </Col>
+      </Row>
+    </MainLayout>
+  );
 };
